519ead9c2487b1ed68cc9e3194c8610b909f21a0
Svipur <71509002+Svipur@users.noreply.github.com>
2020-10-07 01:31:20 +0300

    Hostages, 3 more versions.

--- src/burn/drv/spectrum/d_spectrum.cpp
+++ src/burn/drv/spectrum/d_spectrum.cpp
@@ -11592,56 +11592,56 @@ struct BurnDriver BurnSpectyazzie = {
 // Hostages
 
 static struct BurnRomInfo SpechostagesRomDesc[] = {
-	{ "Hostages (1990)(Infogrames).tap", 222554, 0x171b72b5, BRF_ESS | BRF_PRG },
+	{ "Hostages (1990)(Infogrames).tap", 226915, 0x3d68bbda, BRF_ESS | BRF_PRG },
 };
 
-STDROMPICKEXT(Spechostages, Spechostages, Spec128)
+STDROMPICKEXT(Spechostages, Spechostages, Spectrum)
 STD_ROM_FN(Spechostages)
 
 struct BurnDriver BurnSpechostages = {
-	"spec_hostages", NULL, "spec_spec128", NULL, "1990",
-	"Hostages\0", NULL, "Infogrames", "ZX Spectrum",
+	"spec_hostages", NULL, "spec_spectrum", NULL, "1990",
+	"Hostages 48k\0", NULL, "Infogrames", "ZX Spectrum",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING, 1, HARDWARE_SPECTRUM, GBF_MISC, 0,
 	SpectrumGetZipName, SpechostagesRomInfo, SpechostagesRomName, NULL, NULL, NULL, NULL, SpecInputInfo, SpecDIPInfo,
+	TAPInit, SpecExit, SpecFrame, SpecDraw, SpecScan,
+	&SpecRecalc, 0x10, 288, 224, 4, 3
+};
+
+// hostages1
+
+static struct BurnRomInfo Spechostages1RomDesc[] = {
+	{ "hostages11.tap", 222488, 0x2b5d9b19, BRF_ESS | BRF_PRG },
+};
+
+STDROMPICKEXT(Spechostages1, Spechostages1, Spec128)
+STD_ROM_FN(Spechostages1)
+
+struct BurnDriver BurnSpechostages1 = {
+	"spec_hostages1", NULL, "spec_spec128", NULL, "1990",
+	"hostages1 48k\0", NULL, "Infogrames", "ZX Spectrum",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING, 1, HARDWARE_SPECTRUM, GBF_MISC, 0,
+	SpectrumGetZipName, Spechostages1RomInfo, Spechostages1RomName, NULL, NULL, NULL, NULL, SpecInputInfo, SpecDIPInfo,
 	TAP128KInit, SpecExit, SpecFrame, SpecDraw, SpecScan,
 	&SpecRecalc, 0x10, 288, 224, 4, 3
 };
 
-// Action Force II
+// hostages2
 
-static struct BurnRomInfo Specactionforce2RomDesc[] = {
-	{ "Action Force II (1988)(Virgin Games).tap", 46813, 0x3d4139b7, BRF_ESS | BRF_PRG },
+static struct BurnRomInfo Spechostages2RomDesc[] = {
+	{ "hostages21.tap", 229626, 0x7f97e1da, BRF_ESS | BRF_PRG },
 };
 
-STDROMPICKEXT(Specactionforce2, Specactionforce2, Spec128)
-STD_ROM_FN(Specactionforce2)
+STDROMPICKEXT(Spechostages2, Spechostages2, Spec128)
+STD_ROM_FN(Spechostages2)
 
-struct BurnDriver BurnSpecactionforce2 = {
-	"spec_actionforce2", NULL, "spec_spec128", NULL, "1988",
-	"Action Force II\0", NULL, "Virgin Games", "ZX Spectrum",
+struct BurnDriver BurnSpechostages2 = {
+	"spec_hostages2", NULL, "spec_spec128", NULL, "1990",
+	"hostages2 48k\0", NULL, "Infogrames", "ZX Spectrum",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING, 1, HARDWARE_SPECTRUM, GBF_MISC, 0,
-	SpectrumGetZipName, Specactionforce2RomInfo, Specactionforce2RomName, NULL, NULL, NULL, NULL, SpecInputInfo, SpecDIPInfo,
-	TAP128KInit, SpecExit, SpecFrame, SpecDraw, SpecScan,
-	&SpecRecalc, 0x10, 288, 224, 4, 3
-};
-
-// Action Reflex
-
-static struct BurnRomInfo SpecactionreflexRomDesc[] = {
-	{ "Action Reflex (1986)(Mirrorsoft).tap", 41597, 0xaada6209, BRF_ESS | BRF_PRG },
-};
-
-STDROMPICKEXT(Specactionreflex, Specactionreflex, Spec128)
-STD_ROM_FN(Specactionreflex)
-
-struct BurnDriver BurnSpecactionreflex = {
-	"spec_actionreflex", NULL, "spec_spec128", NULL, "1986",
-	"Action Reflex\0", NULL, "Mirrorsoft", "ZX Spectrum",
-	NULL, NULL, NULL, NULL,
-	BDF_GAME_WORKING, 1, HARDWARE_SPECTRUM, GBF_MISC, 0,
-	SpectrumGetZipName, SpecactionreflexRomInfo, SpecactionreflexRomName, NULL, NULL, NULL, NULL, SpecInputInfo, SpecDIPInfo,
+	SpectrumGetZipName, Spechostages2RomInfo, Spechostages2RomName, NULL, NULL, NULL, NULL, SpecInputInfo, SpecDIPInfo,
 	TAP128KInit, SpecExit, SpecFrame, SpecDraw, SpecScan,
 	&SpecRecalc, 0x10, 288, 224, 4, 3
 };
